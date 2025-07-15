36ff9457f003157245981de59106dd92a5f882d4
jhawkins311 <hawkinsxjasmine@gmail.com>
2025-07-15 11:42:37 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,20 +1,9 @@
-# Streamlit Synthetic Data Evaluation Tool (Single Page Version)
-# Streamlit Synthetic Data Evaluation Tool (Single Page Version)
-import streamlit as st
-import pandas as pd
-import time
-
-from sdv.metadata import SingleTableMetadata
-from sdv.single_table import (
-    CTGANSynthesizer,
-    TVAESynthesizer,
-    GaussianCopulaSynthesizer
-)
-from sdv.evaluation.single_table import evaluate_quality, run_diagnostic
+# Home Page
 
 # Title and Introduction
 st.set_page_config(page_title="Synthetic Data Evaluation Tool")
+
 st.title("Synthetic Data Generator & Evaluator Tool")
+
 st.markdown("""
-Welcome! This is my awesome Capstone tool for generating and evaluating synthetic data. 
-""")
+Welcome! This is my awesome Capstone tool for generating and evaluating synthetic data. """)
