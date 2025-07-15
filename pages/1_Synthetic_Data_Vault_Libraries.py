21f2305a1c0c0fba600f4ba6b1d36decbab2bedd
jhawkins311 <hawkinsxjasmine@gmail.com>
2025-07-15 14:15:40 -0400

    Update 1_Synthetic_Data_Vault_Libraries.py

--- pages/1_Synthetic_Data_Vault_Libraries.py
+++ pages/1_Synthetic_Data_Vault_Libraries.py
@@ -12,11 +12,11 @@ Welcome to **Module 1** of our Synthetic Data 101 course!
 
 Here, you'll explore three generative models from the **[Synthetic Data Vault (SDV)](https://docs.sdv.dev/)** library: 
 
-### (1) GaussianCopula - The Gaussian Copula Synthesizer uses classic statistiical ML methods to learn from real data. It is fast and produces quality results, but it specializes on numerical-only datasets. 
+### (1) CTGAN - "The CTGAN Synthesizer uses GAN-based, deep learning methods to learn from real data. It is the benchmark library used to produce synthetic data with high fidelity. However, it takes the longest. 
 
 ### (2) TVAE - The TVAE Synthesizer uses a variational autoencoder (VAE)-based, neural network techniques to learn from real data. It has a medium speed and increased quality, but it takes longer to learn.
 
-### (3) CTGAN - "The CTGAN Synthesizer uses GAN-based, deep learning methods to learn from real data. It is the benchmark library used to produce synthetic data with high fidelity. However, it takes the longest. 
+### (3) GaussianCopula - The Gaussian Copula Synthesizer uses classic statistiical ML methods to learn from real data. It is fast and produces quality results, but it specializes on numerical-only datasets. 
 
 
 These models are basic table synthesizers. This means the model can process a single table of data and output a synethetic verision. 
