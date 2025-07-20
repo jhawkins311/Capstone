f2e6514b56ecdba97bd400084e46672e47786d6c
jhawkins311 <hawkinsxjasmine@gmail.com>
2025-07-19 23:31:32 -0400

    Update 2_Examples_of_Synthetic_Data.py

--- pages/2_Examples_of_Synthetic_Data.py
+++ pages/2_Examples_of_Synthetic_Data.py
@@ -7,8 +7,8 @@ tabs = st.tabs(["Uploading", "Modeling", "Generating", "Evaluating"])
 
 with tabs[0]:
     st.header("Uploading an Original Dataset")
-    st.image("assets/adult_metadata.png", caption="Metadata Visualization")
-    st.dataframe(pd.read_csv("assets/adult_real_preview.csv").head())
+    #st.image("assets/adult_metadata.png", caption="Metadata Visualization")
+   # st.dataframe(pd.read_csv("assets/adult_real_preview.csv").head())
 
 with tabs[1]:
     st.header("Creating Generative AI Models")
@@ -21,9 +21,9 @@ synthetic_data = ctgan.sample(num_rows)
 
 with tabs[2]:
     st.header("Generating Synthetic Datasets")
-    st.dataframe(pd.read_csv("assets/adult_synthetic_ctgan.csv").head())
+    #st.dataframe(pd.read_csv("assets/adult_synthetic_ctgan.csv").head())
 
 with tabs[3]:
     st.header("Evaluating Synthetic Datasets")
-    st.image("assets/adult_quality_report.png", caption="Quality Score Comparison")
-    st.image("assets/adult_loss_plot.png", caption="Loss Plot During Training")
+    #st.image("assets/adult_quality_report.png", caption="Quality Score Comparison")
+    #st.image("assets/adult_loss_plot.png", caption="Loss Plot During Training")
